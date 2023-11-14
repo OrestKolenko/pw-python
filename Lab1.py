729a69ec1cd346b72d66d015be660af9b47770a2
Orest Kolenko <X-7759@DC=sntis,DC=local>
2023-11-14 13:14:32 +0100

    .

--- Lab1.py
+++ Lab1.py
@@ -21,3 +21,22 @@ date = datetime.now()
 print(date)
 print(date.year)
 print(date.strftime("%Y"))
+
+print(date.strftime("%B"))
+
+print(date.strftime("%A"))
+
+date_object = datetime.strptime("2023-11-15", "%Y-%m-%d")
+print(date_object)
+
+date_object = datetime.strptime("2023-Nov-15", "%Y-%b-%d")
+print(date_object)
+
+date_object = datetime.strptime("2023-Nov-15", "%Y-%b-%d")
+new_date = date_object + timedelta(days=5)
+print(new_date)
+
+print(date-timedelta(weeks=2))
+
+new_year = datetime.strptime("2023-Jan-1", "%Y-%b-%d")
+print((date - new_year).days)
