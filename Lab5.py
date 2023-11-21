8d6b19dfff20072c6fbabc83b94269bb61c66d82
Orest Kolenko <X-7759@DC=sntis,DC=local>
2023-11-21 13:58:37 +0100

    .

--- Lab5.py
+++ Lab5.py
@@ -49,5 +49,38 @@ print("Waga:{}".format(interpret_bmi(result)))
 
 
 
+def check_float(number:float) -> bool:
+    try: 
+        float(number)
+        return True
+    except ValueError:
+        return False
+    
+
+def index_mass(mass:float, height:int) -> float:
+    bmi = mass/(height**2)
+    return index_mass_class(bmi)
+
+
+def index_mass_class(bmi:float) -> str:
+    if bmi <= 18.5:
+        return "You are thin"
+    elif 18.5 < bmi <= 25:
+        return "You have normal weight"
+    elif 25 < bmi <= 30:
+        return "You are overweight"
+    else:
+        return "You are obese"
+
+try:
+    mass = input("Enter your mass: ")
+    height = input("Enter your height: ")
+    if check_float(mass) and height.isdigit():
+        print(index_mass(float(mass), int(height)))
+    else:
+        print("Please enter digits only")
+except ValueError:
+    print("Invalid input")
+
 
 
